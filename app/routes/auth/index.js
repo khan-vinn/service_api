d0d4a9bfca11f7e5c48ae93f4febedd07ad8baf7
Shakhan <khan.vinn@gmail.com>
2021-07-27 17:57:59 +0500

    auth routes dummy

--- app/routes/auth/index.js
+++ app/routes/auth/index.js
@@ -1,6 +1,10 @@
 export default function (app) {
-    app.route("/login")
-    app.route("/logout")
-    app.route("/regiseter")
-    app.route("/verification/:token/:hash")
+    app.route("/auth/login")
+        .get((req, res) => { res.send("login") })
+    app.route("/auth/logout")
+        .get((req, res) => { res.send("logout") })
+    app.route("/auth/register")
+        .get((req, res) => { res.send("regiseter") })
+    app.route("/auth/verification/:token/:hash")
+        .get((req, res) => { res.send(`verification/${req.params.token}/${req.params.hash}`) })
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,9 @@ export default function (app) {
     app.route("/").get((req, res, next) => {
         res.render("index")
     })
+    
     authRouter(app)
+
     app.use((req, res, next) => {
         res.status(401).type("txt").send("Not Found")
     })
